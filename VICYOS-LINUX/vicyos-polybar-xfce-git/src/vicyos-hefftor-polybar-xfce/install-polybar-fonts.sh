efb5522bdd8df3440d482e648183a4ea0c1fb3b7
felipendc <felipendc10@gmail.com>
2020-02-07 12:05:46 -0300

    update

--- VICYOS-LINUX/vicyos-polybar-xfce-git/src/vicyos-hefftor-polybar-xfce/install-polybar-fonts.sh
+++
@@ -1,9 +0,0 @@
-############################
-# Script made by Felipe Ndc
-############################
-
-# This command will install the proper fonts:
-sudo cp -r fonts/* /usr/share/fonts/
-
-# This command will refresh the cache:
-fc-cache -fv
